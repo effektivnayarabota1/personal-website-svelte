2c39fbdc76eb31b114d4f1c53801338b29c0d21e
Kirill Ivanov <effektivnayarabota1@gmail.com>
2022-06-23 19:52:01 +0300

    feat: image block WO JS

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,3 +1,96 @@
-<h1>
-	Image
-</h1>
+<script>
+	import Text from "./Image_text.svelte";
+</script>
+
+<section class="grid">
+	<figure class="image">
+		<picture>
+			<img src="vertical.jpg" alt="" />
+		</picture>
+		<figcaption>
+			<Text />
+		</figcaption>
+	</figure>
+</section>
+<section class="grid">
+	<figure class="image">
+		<picture>
+			<img src="horizontal.jpg" alt="" />
+		</picture>
+		<figcaption>
+			<Text />
+		</figcaption>
+	</figure>
+</section>
+<section class="grid">
+	<figure class="image">
+		<picture>
+			<img src="square.jpg" alt="" />
+		</picture>
+		<figcaption>
+			<Text />
+		</figcaption>
+	</figure>
+</section>
+
+<style>
+	figure {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		gap: var(--gap);
+		grid-column: 2 / 5;
+	}
+	picture {
+		max-height: 100vh;
+		max-width: 100%;
+		padding: var(--padding) 0;
+		/* align-self: flex-end; */
+	}
+	img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+	figcaption {
+		padding: calc(var(--padding) + 8px) 8px 0;
+		min-width: 600px;
+		width: 600px;
+	}
+	@media (max-width: 1919px) {
+		figure {
+			grid-column: 1/4;
+		}
+	}
+	@media (max-width: 1715px) {
+		/* TODO Не получается сделать перебрасыание текстового блока без JS */
+		figure {
+			grid-column: 1/3;
+			flex-wrap: wrap;
+			justify-content: flex-start;
+			align-content: flex-start;
+			gap: 0;
+		}
+		picture {
+			padding: var(--padding) 0 0;
+			max-height: calc(100vh - var(--padding));
+		}
+		figcaption {
+			padding: 0 8px 0;
+			min-width: auto;
+			max-width: 600px;
+		}
+	}
+	@media (max-width: 1287px) {
+		figure {
+			grid-column: 1/4;
+		}
+	}
+	@media (max-width: 1023px) {
+	}
+	@media (max-width: 767px) {
+		figcaption {
+			width: 100%;
+		}
+	}
+</style>
---
+++ src/components/Image_text.svelte
@@ -0,0 +1,36 @@
+<div class="indicator">
+	<img src="navigation/text-indicator.svg" alt="unread text indicator" />
+</div>
+<h1>Форма и Цвет храма.</h1>
+<p>
+	Храм был высечен из цельного куска белой мраморной скалы с вкраплениями жил
+	большого количества разных пород. Уникальные сочетания материалов добавляли
+	храму природного величия. Тщательная полировка заставляла блестеть эти
+	минералы и руды. Много путников пало, пытаясь ощутить могущество этого
+	вечного здания. Было очевидно, что создать красоту такого масштаба без
+	помощи божества простым архитекторам было просто невозможно.
+</p>
+<p class="desc">105x75, акрил, холст лен</p>
+
+<style>
+	h1 {
+		margin-bottom: 24px;
+	}
+	p {
+		margin-bottom: 16px;
+	}
+	.indicator {
+		width: 80px;
+		height: 80px;
+		padding-top: 8px;
+
+		display: none;
+	}
+	@media (max-width: 1715px) {
+		.indicator {
+			display: flex;
+			align-items: center;
+			flex-direction: column;
+		}
+	}
+</style>
